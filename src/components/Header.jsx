441ec7541d50299e519210f46ad0f323dc1af7e7
ShubhamKandpal17 <shubhamkandpal017@gmail.com>
2023-08-29 22:20:37 +0530

    Changed UI and QoL changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import HighlightIcon from "@material-ui/icons/Highlight";
 
 function Header() {
   return (
     <header>
       <h1>
-        <HighlightIcon />
-        Keeper
+      <svg class= "icon" xmlns="http://www.w3.org/2000/svg" color="white" enable-background="new 0 0 30 30" height="26" viewBox="0 0 20 20" width="26"><rect fill="none" height="28" width="28"/><path d="M3,10h11v2H3V10z M3,8h11V6H3V8z M3,16h7v-2H3V16z M18.01,12.87l0.71-0.71c0.39-0.39,1.02-0.39,1.41,0l0.71,0.71 c0.39,0.39,0.39,1.02,0,1.41l-0.71,0.71L18.01,12.87z M17.3,13.58l-5.3,5.3V21h2.12l5.3-5.3L17.3,13.58z"/></svg>
+        Note Diary
       </h1>
     </header>
   );
